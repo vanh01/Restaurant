e5f02acdde103bd1b677214f76de3f60973240ef
vanh01 <ngvanhfw@gmail.com>
2021-11-03 19:32:21 +0700

    nav-bả

--- ClientApp/src/components/Navbar/NavbarCustomer.jsx
+++ ClientApp/src/components/Navbar/NavbarCustomer.jsx
@@ -2,65 +2,47 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    function HandleOnClick(e) {
-        // let list = document.querySelectorAll(".nav-item");
-        // for (let i = 0; i < list.length; i++) {
-        //     list[i].className = "nav-item";
-        // }
-        // e.target.className = "nav-item active";
-    }
-
     return (
         <>
             <div className="nav-behind"></div>
             <nav className="nav-bar">
                 <ul>
-                    <li className="nav-item">
+                    <li>
+                        <i className="fas fa-store"></i>
+                    </li>
+                    <li className="active">
+                        <b></b>
+                        <b></b>
                         <div>
-                            <i className="fas fa-store"></i>
+                            <Link to="/">
+                                <i className="fas fa-home-alt"></i>
+                            </Link>
                         </div>
                     </li>
-                    <li className="nav-item" onClick={HandleOnClick}>
-                        <Link to="/">
-                            <i className="fas fa-home-alt"></i>
-                            <span>Home</span>
-                        </Link>
+                    <li className="">
+                        <b></b>
+                        <b></b>
+                        <div>
+                            <Link to="/DashBoard">
+                                <i className="fas fa-chart-pie-alt"></i>
+                            </Link>
+                        </div>
                     </li>
-                    <li className="nav-item" onClick={HandleOnClick}>
-                        <Link to="/DashBoard">
-                            <i className="fas fa-chart-pie-alt"></i>
-                            <span>Dash board</span>
-                        </Link>
+                    <li className="">
+                        <b></b>
+                        <b></b>
+                        <div>
+                            <Link to="/Setting">
+                                <i className="fas fa-cog"></i>
+                            </Link>
+                        </div>
                     </li>
-                    {/* <li className="nav-item">
-                        <a href="/History">
-                            <i class="fas fa-history"></i>
-                            <span>History</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#">
-                            <i className="fas fa-bell"></i>
-                            <span>Notify</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#">
-                            <i className="fas fa-envelope"></i>
-                            <span>Message</span>
-                        </a>
-                    </li> */}
-                    <li className="nav-item" onClick={HandleOnClick}>
-                        <Link to="/Setting">
-                            <i className="fas fa-cog"></i>
-                            <span>Setting</span>
-                        </Link>
-                    </li>
-                    <li className="nav-item" onClick={HandleOnClick}>
-                        <Link to="/Home">
-                            <i className="fas fa-sign-out"></i>
-                            <span>Logout</span>
-                        </Link>
+                    <li>
+                        <div>
+                            <Link to="/Home">
+                                <i className="fas fa-sign-out"></i>
+                            </Link>
+                        </div>
                     </li>
                 </ul>
             </nav>
