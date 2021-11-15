77b3989d05189826a9113e2ba80aa01b55c2c4d1
vanh01 <ngvanhfw@gmail.com>
2021-11-15 20:48:25 +0700

    update file sqlexecutes

--- Models/Account.cs
+++ Models/Account.cs
@@ -9,7 +9,7 @@ namespace RestaurantPOS2._0
         public string Password { get; set; }
         public string Fname { get; set; }
         public string LName { get; set; }
-        public string BirthOfDate { get; set; }
+        public DateTime BirthOfDate { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public string Img { get; set; }
--- SqlExecutes.cs
+++ SqlExecutes.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using System.Collections.Generic;
 using System;
 using System.Data.SqlClient;
 using System.Data;
@@ -14,25 +16,74 @@ namespace RestaurantPOS2._0
             private set { }
         }
 
-        public SqlExecutes()
-        {
-        }
+        public SqlExecutes() { }
 
         public DataTable ExecuteQuery(string query)
         {
-            SqlConnection myCon = new SqlConnection(sqlDataSource);
             DataTable table = new DataTable();
-            SqlDataReader myReader;
-            myCon.Open();
-            using (SqlCommand myCommand = new SqlCommand(query, myCon))
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
             {
-                myReader = myCommand.ExecuteReader();
-                table.Load(myReader);
+                SqlDataReader myReader;
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
 
-                myReader.Close();
-                myCon.Close();
+                    myReader.Close();
+                    myCon.Close();
+                }
             }
             return table;
         }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int numberOfRows = 0;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    numberOfRows = myCommand.ExecuteNonQuery();
+                    myCon.Close();
+                }
+            }
+            return numberOfRows;
+        }
     }
+
+
+    public static class TableExtensions
+    {
+        public static List<T> ConvertToList<T>(this DataTable dataTable)
+        {
+            List<T> data = new List<T>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+        private static T GetItem<T>(DataRow dr)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (DataColumn column in dr.Table.Columns)
+            {
+                foreach (PropertyInfo pro in temp.GetProperties())
+                {
+                    if (pro.Name == column.ColumnName)
+                        pro.SetValue(obj, dr[column.ColumnName], null);
+                    else
+                        continue;
+                }
+            }
+            return obj;
+        }
+    }
+
+
 }
